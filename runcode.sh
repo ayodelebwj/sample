cf2041a0640078cf761e6d68e4f6d23c2c09e044
olaniyi <ayodelebwj@gmail.com>
2026-02-03 18:11:08 -0500

    push12

--- runcode.sh
+++
@@ -1,4 +0,0 @@
-#!bin/bash
-terraform apply auto-approve -var-file =./terraform/variables.tfvars
-terraform output -json > ./ansible/tf_outputs.json
-ansible-playbook -i ./ansible/inventory/hosts.ini ./ansible/playbook.yml
