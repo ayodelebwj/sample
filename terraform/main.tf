9ec958aa6768ceb2add1a0ea4f9e3bec0a73e6d6
Your NameOlaniyi <ayodelebwj@gmail.com>
2026-02-03 13:39:39 -0500

    push3

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,6 @@
 terraform {
     backend "s3" {
-        bucket = "techbleatweek8"
+        bucket = "techbleatweek10b"
         key = "env/prod/terraform.tfstate"
         encrypt = true
         region = "us-east-1"
